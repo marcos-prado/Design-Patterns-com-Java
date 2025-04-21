583f6bc807d7f7046e052bc480f9022cb8a04b74
marcos-prado <71200133+marcos-prado@users.noreply.github.com>
2025-04-21 20:04:33 -0300

    Update LabPadroesProjetoSpringApplication.java

--- src/main/java/one/digitalinovation/gof/LabPadroesProjetoSpringApplication.java
+++ src/main/java/one/digitalinovation/gof/LabPadroesProjetoSpringApplication.java
@@ -4,17 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-/*	
- * Projeto Spring Boot gerado via Spring Initializr
- * https://start.spring.io/
- * Os seguintes módulos foram adicionados:
- * - Spring Web
- * - Spring Data JPA
- * - H2 Database
- * - OpenFeign
- * 
- * @autor marcos.prado
- */
 @EnableFeignClients
 @SpringBootApplication
 public class LabPadroesProjetoSpringApplication {
